f87181553a95110228c44d07d085754b30630906
ajonthephone <ajonthephone@gmail.com>
2021-02-24 16:37:51 -0500

    update
    update of content

---
+++ Test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        // GET: api/<Test>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string[] animals = { "Lion", "Tiger", "Elephant" };
+            
+            { return animals; }
+     
+        }
+        
+        // GET api/<Test>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Test>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Test>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Test>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
